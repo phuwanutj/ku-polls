4df02ceb7109ffd831eb6ff6e80158cdab9c4baf
unknown <phuwanut.j@ku.th>
2020-09-18 10:03:18 +0700

    Update redirect

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question,Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class IndexView(generic.ListView):
@@ -13,15 +14,23 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    def get(self, request, *args, **kwargs):
+        try:
+            question = Question.objects.get(pk=kwargs['pk'])
+            if not question.can_vote():
+                return HttpResponseRedirect(reverse('polls:index'), messages.error(request, "You can't vote on this poll because this poll is already ended."))
+        except ObjectDoesNotExist:
+            return HttpResponseRedirect(reverse('polls:index'), messages.error(request, "Poll does not exist."))
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 class ResultsView(generic.DetailView):
